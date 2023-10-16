5b55d9d86d0a202bc44836dfe2d831bcfb77fdf6
aadimach2 <147931709+aadimach2@users.noreply.github.com>
2023-10-17 04:49:34 +0530

    Update index.js

--- To Do List/index.js
+++ To Do List/index.js
@@ -6,7 +6,7 @@ const container=document.querySelector(".container");
 const tasks=localStorage.getItem("tasks")
  ?  JSON.parse(localStorage.getItem("tasks"))
  :[];
- ShowAllTasks();
+ ShowAllTask();
 
 
 
